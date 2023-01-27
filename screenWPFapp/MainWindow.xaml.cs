5e5ea42ab626148945357a2b619dc7708f4b072c
X-dante-X <solomamd17@gmail.com>
2023-01-27 20:12:07 +0100

    zxc

--- screenWPFapp/MainWindow.xaml.cs
+++ screenWPFapp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+
 
 namespace screenWPFapp
 {
@@ -23,6 +27,58 @@ namespace screenWPFapp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ActiveWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rect = new RECT();
+            GetWindowRect(GetForegroundWindow(), ref rect);
+            using (Bitmap bmp = new Bitmap(rect.Width, rect.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(rect.X, rect.Y, 0, 0, bmp.Size);
+                }
+                SaveScreenshot(bmp);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, ref RECT rect);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int X;
+            public int Y;
+            public int Width;
+            public int Height;
+        }
+        private void FullScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+            using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                }
+                SaveScreenshot(bmp);
+            }
+        }
+
+        private void SaveScreenshot(Bitmap screenshot)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG|*.png|JPEG |.jpg;.jpeg | BMP |.bmp | GIF |.gif";
+            saveFileDialog.Title = "Save Screenshot";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                screenshot.Save(saveFileDialog.FileName);
+            }
         }
     }
 }
