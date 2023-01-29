b405000ca5426dc5517c74b46b55bd757f9043f3
X-dante-X <solomamd17@gmail.com>
2023-01-29 13:39:13 +0100

    close/open

--- screenWPFapp/MainWindow.xaml.cs
+++ screenWPFapp/MainWindow.xaml.cs
@@ -15,18 +15,73 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
-
+using System.Windows.Interop;
 
 namespace screenWPFapp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const int HotkeyModifiers = 0x0000;
+        private const int Hotkey1 = 0x31;
+        private const int Hotkey2 = 0x32;
+
+        private HwndSource hwndSource;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
         public MainWindow()
         {
+            
             InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.Closing += MainWindow_Closing;
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var windowInteropHelper = new WindowInteropHelper(this);
+            hwndSource = HwndSource.FromHwnd(windowInteropHelper.Handle);
+            hwndSource.AddHook(WndProc);
+
+            RegisterHotKey(hwndSource.Handle, Hotkey1, HotkeyModifiers, (int)Key.NumPad5);
+            RegisterHotKey(hwndSource.Handle, Hotkey2, HotkeyModifiers, (int)Key.NumPad6);
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UnregisterHotKey(hwndSource.Handle, Hotkey1);
+            UnregisterHotKey(hwndSource.Handle, Hotkey2);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0312)
+            {
+                int id = wParam.ToInt32();
+                if (id == Hotkey1)
+                {
+                    this.Activate();
+                    this.Topmost = true;
+                }
+                if (id == Hotkey2)
+                {
+                    this.Close();
+                }
+                handled = true;
+            }
+            return IntPtr.Zero;
         }
 
         private void ActiveWindowButton_Click(object sender, RoutedEventArgs e)
